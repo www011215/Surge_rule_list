cfd157b9f1b2984eebfcc1af08ce323407783d82
wwb011215 <65267198+wwb011215@users.noreply.github.com>
2025-02-10 16:47:14 +0800

    Update auto_login.js

--- auto_login.js
+++ auto_login.js
@@ -1,9 +1,9 @@
-## Surge HTTP Request Hook: 自动填充登录请求
+// Surge HTTP Request Hook: 自动填充登录请求
 let url = $request.url;
 let headers = $request.headers;
 let body = $request.body;
 
-## 目标网站的登录 API
+// 目标网站的登录 API
 if (url.includes("transmedlims.pumch.cn/Account/login")) {
     let newBody = "userName=S2024001046&password=Wwb@13306120968"; // 替换为你的账户信息
     let newHeaders = {
